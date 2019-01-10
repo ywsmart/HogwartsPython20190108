d3159175e60f2fe4888c74f9c3e09d97b0c67f20
ywsmart <ywsmart@163,com>
2019-01-11 01:31:32 +0800

    整理代码

--- src/testcases/api/test_pyexample_1.py
+++ src/testcases/api/test_pyexample_1.py
@@ -3,14 +3,17 @@
 import time
 import pytest
 
-def add(x,y):
-    return x+y
+
+def add(x, y):
+    return x + y
+
 
 def test_add():
     assert add(1, 2) == 3
 
+
 def test_add2():
     print("I am 2")
     time.sleep(3)
-    assert  add(1.2, 3.1) == 5.3
-    assert add(1,2) == 3
+    assert add(1.2, 3.1) == 5.3
+    assert add(1, 2) == 3
